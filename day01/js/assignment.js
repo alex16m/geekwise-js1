60eb78b17502d58834328a8b13693a56b373796d
Alex Moun <Alex Moun>
2017-05-15 20:57:32 -0700

    day01

---
+++ day01/js/assignment.js
@@ -0,0 +1,12 @@
+var firstName = prompt("Please enter your first name: ");
+var lastName = prompt("Please enter your last name: ");
+var age = prompt("Please enter your age here: ");
+var place = prompt("Please enter a place: ");
+
+if(place != null) {
+    confirm("Press Ok if <" + place, + "> is the correct place");
+}
+
+alert("Your first name: " + firstName + "\n Your last name: " + lastName);
+
+console.log(age);
---
+++ day01/js/script.js
@@ -0,0 +1,47 @@
+// ES 6 - ECMAScript6
+
+// good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+console.log("sending a messaage to ocnsole.");
+console.info("Sending information message");
+console.warn("here's a warning");
+console.error("here's an error");
+
+
+// Single line comment
+
+/*
+    Multi line comment
+*/
+
+var
+
+
+
+                                                freedom
+
+
+                    =
+
+
+
+
+                                "A string of Javascript";
+console.log(freedom);
+
+// template string
+var javascriptTemplateString = `
+and
+            type
+    whatever
+    you
+    want
+    :)
+`;
+
+console.log(javascriptTemplateString);
