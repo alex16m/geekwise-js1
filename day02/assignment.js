787aaa570fbc3f004798b9940e7fa88131799765
Alex Moun <Alex Moun>
2017-05-19 13:42:47 -0700

    assignment on day02 is finished

---
+++ day02/assignment.js
@@ -0,0 +1,6 @@
+// var randNumFromOneToHundred = Math.floor
+
+console.log( Math.floor( Math.random() * 99) + 1);
+
+console.log( Math.floor( Math.random() * 100));
+// console.log( Math.floor( Math.random() * 11) );
